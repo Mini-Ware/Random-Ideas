7f9504f0682de29f445169f714a00b4c305d24dd
Mini-Ware <codauknow@gmail.com>
2021-08-14 22:43:58 +0800

    Updated 7up.py

---
+++ 7up.py
@@ -0,0 +1,14 @@
+current = 1
+next = 0
+
+#Start counting
+while True:
+  print("Current number: "+str(current))
+  next = input("Next number: ")
+
+  #Confirm if its a multiple
+  if (current + 1) % 7 == :
+    #Up should be next
+
+  else:
+    #Make sure its correct
