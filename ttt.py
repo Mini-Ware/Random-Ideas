4328e793a71876430cdd9c037dc30b5e3f5db46c
Mini-Ware <codauknow@gmail.com>
2021-08-15 14:28:03 +0800

    Updated ttt.py

--- ttt.py
+++ ttt.py
@@ -12,9 +12,20 @@ while progress:
   choice = int(input("Select a square (1-9): "))-1
 
   #Update board
-  print(str(choice))
+  if choice > 5:
+    choice = choice - 6
+    board[2][choice] = "X "
+  elif choice > 2:
+    choice = choice - 3
+    board[1][choice] = "X "
+  else:
+    board[0][choice] = "X "
 
   #Response
   print(" —— —— ——\n|"+board[0][0]+"|"+board[0][1]+"|"+board[0][2]+"|\n —— —— ——\n|"+board[1][0]+"|"+board[1][1]+"|"+board[1][2]+"|\n —— —— ——\n|"+board[2][0]+"|"+board[2][1]+"|"+board[2][2]+"|\n —— —— ——")
 
-  #Conditions
+  #Game over
+  if :
+    print("You won!")
+  elif :
+    print("You lost!")
