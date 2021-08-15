e32d470a6e0c0d0daa353987301840001a5aaec5
Mini-Ware <codauknow@gmail.com>
2021-08-16 02:20:32 +0800

    Updated ttt.py

--- ttt.py
+++ ttt.py
@@ -8,30 +8,8 @@ board = [["  ","  ","  "],
          ["  ","  ","  "],
          ["  ","  ","  "]]
 
-#Main loop
-while progress:
-  position = int(input("Select a square (1-9): "))-1
-  x = 0
-  y = 0
-
-  #Update board
-  if position > 5:
-    x = position - 6
-    y = 2
-  elif position > 2:
-    x = position - 3
-    y = 1
-  else:
-    x = position
-    y = 0
-
-  if board[y][x] == "  ":
-    board[y][x] = "X "
-  else:
-    print("That square has already been occupied!")
-
-  #Response
-  print(" —— —— ——\n|"+board[0][0]+"|"+board[0][1]+"|"+board[0][2]+"|\n —— —— ——\n|"+board[1][0]+"|"+board[1][1]+"|"+board[1][2]+"|\n —— —— ——\n|"+board[2][0]+"|"+board[2][1]+"|"+board[2][2]+"|\n —— —— ——")
+#Functions
+def check(progress, board):
 
   #Game over
   loopx = 0
@@ -71,9 +49,72 @@ while progress:
     result = "lose"
     progress = False
 
+  #Check draw
+  if board[0][0] != "  " and board[0][1] != "  " and board[0][2] != "  " and board[1][0] != "  " and board[1][1] != "  " and board[1][2] != "  " and board[2][0] != "  " and board[2][1] != "  " and board[2][2] != "  ":
+    result = "draw"
+    progress = False
 
   if progress == False:
     if result == "win":
       print("You won!")
     elif result == "lose":
       print("You lost!")
+    elif result == "draw":
+      print("Its a draw!")
+  return progress
+
+#Main loop
+while progress:
+  confirm = False
+  position = int(input("Select a square (1-9): "))-1
+  x = 0
+  y = 0
+
+  #Update board
+  if position > 5:
+    x = position - 6
+    y = 2
+  elif position > 2:
+    x = position - 3
+    y = 1
+  else:
+    x = position
+    y = 0
+
+  if board[y][x] == "  ":
+    board[y][x] = "X "
+  else:
+    print("That square has already been occupied!")
+    confirm = True
+  check(progress, board)
+
+  #Response
+  loopx = 0
+  loopy = 0
+  move = 0
+
+  #Block horizontal
+
+  #Block vertical
+
+  #Block diagonal
+
+  #Anyhow place
+  while confirm == False:
+    move = random.randint(0,8)
+    if move > 5:
+      x = move - 6
+      y = 2
+    elif move > 2:
+      x = move - 3
+      y = 1
+    else:
+      x = move
+      y = 0
+
+    if board[y][x] == "  ":
+      board[y][x] = "O "
+      confirm = True
+
+  check(progress, board)
+  print(" —— —— ——\n|"+board[0][0]+"|"+board[0][1]+"|"+board[0][2]+"|\n —— —— ——\n|"+board[1][0]+"|"+board[1][1]+"|"+board[1][2]+"|\n —— —— ——\n|"+board[2][0]+"|"+board[2][1]+"|"+board[2][2]+"|\n —— —— ——")
