7b7ed5ada5576d6d6cd361f5c5b3fb61626ca79e
Mini Ware <codauknow@gmail.com>
2022-10-31 22:33:51 +0800

    display box based on input, using oop

---
+++ Aesthetic/main.py
@@ -0,0 +1,20 @@
+class _():
+	_=None
+	def __init__(self,_):
+		self._=None
+		self.__class__._=_
+	def _(self,_):
+		self._=_
+	def __str__(self):
+		return self.__class__._
+	def __(self):
+		return self._
+class __(_):
+	def __init__(self,__):
+		super().__init__(__)
+		self._,self.__=len(_.__str__(self)),None
+	def __str__(self):
+		for self.__ in _.__str__(self)[0:-1]:
+			print(super().__str__())
+		return _.__str__(self)
+(lambda __=__(input()):(None,str(__))[isinstance(__,_)])()
